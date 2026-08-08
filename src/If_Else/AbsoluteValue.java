ba2e9b7cfc39f485c2db9eff7be94a8a987f8c12
Shiva Sharma <techofliger@gmail.com>
2026-08-08 23:33:53 +0530

    Basics

--- src/If_Else/AbsoluteValue.java
+++ src/If_Else/AbsoluteValue.java
@@ -2,15 +2,16 @@ package If_Else;
 
 import java.util.Scanner;
 
-public class OddEven {
+public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int n = sc.nextInt();
-        if(n%2==0){
-        System.out.println("The number is even: " + n);
+        if(n<=0){
+            System.out.println(-n + " is absolute value of " + n + ".");
         }else{
-            System.out.println("The number is odd: " + n);
+            System.out.println(n + " is absolute value of " + n + ".");
         }
+
     }
 }
--- src/If_Else/DivisibleByFive.java
+++ src/If_Else/DivisibleByFive.java
@@ -2,15 +2,15 @@ package If_Else;
 
 import java.util.Scanner;
 
-public class OddEven {
+public class DivisibleByFive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int n = sc.nextInt();
-        if(n%2==0){
-        System.out.println("The number is even: " + n);
+        if(n%5==0){
+            System.out.println(n + " is divisible by 5.");
         }else{
-            System.out.println("The number is odd: " + n);
+            System.out.println(n + " is not divisible by 5.");
         }
     }
 }
