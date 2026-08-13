5767bd4d72ce90fff46e372398009f8bc7f51454
Shiva Sharma <techofliger@gmail.com>
2026-08-13 17:20:20 +0530

    Basics

---
+++ src/Loops/EvenHundred.java
@@ -0,0 +1,15 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class EvenHundred {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            if(i%2==0) System.out.println(i+ " ");
+            }
+        }
+    }
+
