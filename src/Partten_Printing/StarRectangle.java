b3e5380d451f03888c753c1555fb2385d6d0de6d
Shiva Sharma <techofliger@gmail.com>
2026-08-20 19:35:32 +0530

    Basics

---
+++ src/Partten_Printing/StarRectangle.java
@@ -0,0 +1,19 @@
+package Partten_Printing;
+
+import java.util.Scanner;
+
+public class StarRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row number: ");
+        int row = sc.nextInt();
+        System.out.print("Enter column number: ");
+        int col = sc.nextInt();
+        for (int i = 1; i <= row; i++) {  // No. of lines
+            for (int j = 1; j <= col; j++) { // How many star
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
